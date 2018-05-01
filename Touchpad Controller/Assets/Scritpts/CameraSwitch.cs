916304583c7a02443fa9512552c05b4183f8502b
Xolution13 <e-lewandowski@web.de>
2018-05-01 12:26:06 +0200

    Finished Camera Movement

--- Touchpad Controller/Assets/Scritpts/CameraSwitch.cs
+++ Touchpad Controller/Assets/Scritpts/CameraSwitch.cs
@@ -5,27 +5,63 @@ using UnityEngine;
 public class CameraSwitch : MonoBehaviour
 {
     public Camera mainCamera;
-    public GameObject firstPersonController;
-    private Camera controllerCamera;
+    public GameObject controllerEnities;
 
-    private void Start()
-    {
-        controllerCamera = firstPersonController.GetComponentInChildren<Camera>();
-    }
+    public Transform controllerTransform;   // Use a copy of the controller camera transform (not as child of controller entities!)
 
-    // Main camera is enabled on awake
+    public float rotationSpeed;
+    public float translationSpeed;
+
+    private bool moveCamera = false;
+    private bool activateController = false;
+
     private void Awake()
     {
-        firstPersonController.SetActive(false);
-        mainCamera.enabled = true;
+        SetActiveAllChildren(controllerEnities.transform, false);   // Disable everything that belongs to the controller
+        mainCamera.enabled = true;                                  // Make sure that the main camera is on
     }
 
-    // Call this method to activate first preson controller
+    private void Update()
+    {
+        // Called when button is clicked
+        if (moveCamera)
+        {
+            // Rotate camera to the controller rotation
+            mainCamera.transform.rotation = Quaternion.RotateTowards(mainCamera.transform.rotation, 
+                                                                    controllerTransform.rotation, 
+                                                                    Time.deltaTime * rotationSpeed * 10);
+            // Move camera to the controller position
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position,
+                                                                controllerTransform.position,
+                                                                Time.deltaTime * translationSpeed * 10);
+
+            // Activate controller when camera has moved to the right position
+            if (mainCamera.transform.position == controllerTransform.position)
+            {
+                moveCamera = false;
+                activateController = true;
+            }
+        }
+        else if (activateController)
+        {
+            SetActiveAllChildren(controllerEnities.transform, true);    // Enable everything that belongs to the controller
+            mainCamera.enabled = false;                                 // Disable main camera
+        }
+    }
+
+    // Call this method on button to activate first person controller
     public void ActivateControllerCamera()
     {
-        firstPersonController.SetActive(true);
-        mainCamera.enabled = false;
-        controllerCamera.enabled = true;
+        moveCamera = true;
     }
 
+    // Check for every child and disable or enable it
+    private void SetActiveAllChildren(Transform transform, bool value)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+            SetActiveAllChildren(child, value);
+        }
+    }
 }

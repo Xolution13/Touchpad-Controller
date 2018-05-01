0a0b4ac05698b88beff17e3f529a228842eefcf7
Xolution13 <e-lewandowski@web.de>
2018-05-01 11:37:39 +0200

    Camera Switch

---
+++ Touchpad Controller/Assets/Scritpts/CameraSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject firstPersonController;
+    private Camera controllerCamera;
+
+    private void Start()
+    {
+        controllerCamera = firstPersonController.GetComponentInChildren<Camera>();
+    }
+
+    // Main camera is enabled on awake
+    private void Awake()
+    {
+        firstPersonController.SetActive(false);
+        mainCamera.enabled = true;
+    }
+
+    // Call this method to activate first preson controller
+    public void ActivateControllerCamera()
+    {
+        firstPersonController.SetActive(true);
+        mainCamera.enabled = false;
+        controllerCamera.enabled = true;
+    }
+
+}
